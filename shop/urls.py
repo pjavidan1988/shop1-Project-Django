d1f2a44a73db96ce6661ea59a7c0ced35bf487e7
peyman <p.javidan1988@gmail.com>
2021-04-17 12:04:44 +0430

    add templates part3

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('shop/', views.shop, name='shop'),
+    path('single-product/', views.single_product, name='single-product')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'index.html')
+
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+
+def shop(request):
+    return render(request, 'shop.html')
+
+
+def single_product(request):
+    return render(request, 'single-product.html')
