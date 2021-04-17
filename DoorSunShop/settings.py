ac8954f83c7699a580e0dbf8f22f3e76b48dd8c9
peyman <p.javidan1988@gmail.com>
2021-04-17 15:34:48 +0430

    create models and database

--- DoorSunShop/settings.py
+++ DoorSunShop/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'DoorSunShop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shop',
+        'USER': 'postgres',
+        'PASSWORD': 'pepe1366',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
