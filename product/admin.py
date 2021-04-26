ae0e5f471727dc4c750658811e1192448b085ad4
peyman <p.javidan1988@gmail.com>
2021-04-26 12:37:46 +0430

    4/26/2021 12:37

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
-from product.models import Category, Product
+from product.models import Category, Product, Picture
 
-admin.site.register(Category)
-admin.site.register(Product)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'parent', 'status']
+    list_filter = ['status']
+
+class ProductImageInline(admin.TabularInline):
+    model = Picture
+    readonly_fields = ('id',)
+    extra = 1
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'status','image_tag']
+    list_filter = ['category']
+    readonly_fields = ('image_tag',)
+    inlines = [ProductImageInline]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Picture)
+
