43cea078bc7d968ad6b677eab26438024e893895
peyman <p.javidan1988@gmail.com>
2021-08-12 19:51:07 +0430

    8/12/2021 19:50

--- order/views.py
+++ order/views.py
@@ -1,7 +1,11 @@
+import json
+
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.sites import requests
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
+import requests
 
 # Create your views here.
 from django.utils.crypto import get_random_string
@@ -117,6 +121,22 @@ def deletefromcart(request, id):
     return HttpResponseRedirect("/shopcart")
 
 
+MERCHANT = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+ZP_API_REQUEST = "https://api.zarinpal.com/pg/v4/payment/request.json"
+ZP_API_VERIFY = "https://api.zarinpal.com/pg/v4/payment/verify.json"
+ZP_API_STARTPAY = "https://www.zarinpal.com/pg/StartPay/{authority}"
+amount = 11000  # Rial / Required
+description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"  # Required
+email = 'p.javidan1988@gmail.com.com'  # Optional
+mobile = '09101524024'  # Optional
+# Important: need to edit for realy server.
+CallbackURL = 'order/orderproduct/'
+
+
+
+
+
+
 def orderproduct(request):
     category = Category.objects.all()
     current_user = request.user
@@ -143,6 +163,26 @@ def orderproduct(request):
         # return HttpResponse(request.POST.items())
         if form.is_valid():
             # Send Credit card to bank,  If the bank responds ok, continue, if not, show the error
+
+            req_data = {
+                "merchant_id": MERCHANT,
+                "amount": amount,
+                "callback_url": CallbackURL,
+                "description": description,
+                "metadata": {"mobile": mobile, "email": email}
+            }
+            req_header = {"accept": "application/json",
+                          "content-type": "application/json'"}
+            req = requests.post(url=ZP_API_REQUEST, data=json.dumps(
+                req_data), headers=req_header)
+            authority = req.json()['data']['authority']
+            if len(req.json()['errors']) == 0:
+                return redirect(ZP_API_STARTPAY.format(authority=authority))
+            else:
+                e_code = req.json()['errors']['code']
+                e_message = req.json()['errors']['message']
+                return HttpResponse(f"Error code: {e_code}, Error Message: {e_message}")
+
             # ..............
 
             data = Order()
@@ -206,6 +246,12 @@ def orderproduct(request):
                'amount':amount
                }
     return render(request, 'Order_Form.html', context)
+
+
+
+
+
+
 
 
 @login_required(login_url='/login')
